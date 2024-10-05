3900ec910a438c14069d975cfe625c886acd2b47
unknown <khalidabdi3101@gmail.com>
2024-10-05 05:24:05 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Good Luck 💪🏾
+function ZeroAndOne(arr) {
+    
+    return arr.map(item => {
+        if (item === 'Zero') return '0';
+        if (item === 'One') return '1';
+        return ''; 
+    }).join(''); 
+}
+
+
+console.log(ZeroAndOne(["one", "Zero", "Zero", "One"])); 
+console.log(ZeroAndOne(["Zero", "ONE", "one", "Zero"])); 
